4ec47449f9148e1708c34358c43aab999f8f40a9
Hane Liu <hane0131@gmail.com>
2015-12-22 22:40:21 +0800

    add first method

--- lib/PerlPDB.pm
+++ lib/PerlPDB.pm
@@ -4,9 +4,15 @@ use warnings;
 
 use Exporter qw(import);
 
-our $VERSION    = 1.0
-our @ISA        = qw(Exporter)
+our $VERSION    = 1.0;
+our @ISA        = qw(Exporter);
 our @EXPORT_OK  = qw(
-    )
+    make_search_query
+    );
 
+sub make_search_query{
+    my (%query) = @_;
+    print "make_search_query\n";
+}
 
+1;
