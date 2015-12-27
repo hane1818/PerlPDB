44e4b82a4c17b9c0c0f26696c09d765b10b8633c
Hane Liu <hane0131@gmail.com>
2015-12-27 12:42:41 +0800

    compare querytype in make_search_query() and prettify code

--- lib/PerlPDB.pm
+++ lib/PerlPDB.pm
@@ -12,15 +12,49 @@ our @EXPORT_OK  = qw(
 
 sub make_search_query {
     my ($key, $type)=@_;
+
     my @querytype = ('HoldingsQuery', 'ExpTypeQuery',
                     'AdvancedKeywordQuery','StructureIdQuery',
                     'ModifiedStructuresQuery', 'AdvancedAuthorQuery', 'MotifQuery',
                     'NoLigandQuery');
-    if ( ! grep { $_ eq $type } @querytype) {
+    my $querytype = join('|', @querytype);
+    if ( $type !~ $querytype ) {
         $type='AdvancedKeywordQuery'; }
+
     my %query;
     $query{'queryType'}=$type;
+    if ( $type =~ /^AdvancedKeywordQuery$/ ) {
+
+        $query{'description'} = 'Text Search for: ' . $key;
+        $query{'keywords'} = $key;
+
+    }elsif ( $type =~ /^NoLigandQuery$/ ) {
+
+        $query{'haveLigands'} = 'yes';
+
+    }elsif ( $type =~ /^AdvancedAuthorQuery$/ ) {
+
+        $query{'description'} = 'Author Name: ' . $key;
+        $query{'searchType'} = 'All Authors';
+        $query{'audit_author.name'} = $key;
+        $query{'exactMatch'} = 'false';
+
+    }elsif ( $type =~ /^MotifQuery$/ ) {
+
+        $query{'description'} = 'Motif Query For: ' . $key;
+        $query{'motif'} = $key;
+
+    }elsif ( $type =~ /^(StructureIdQuery|ModifiedStructuresQuery)$/ ) {
+
+        $query{'structureIdList'} = $key;
+
+    }elsif ( $type =~ /^ExpTypeQuery$/ ) {
+
+        $query{'experimentalMethod'} = $key;
+        $query{'description'} = 'Experimental Method Search : Experimental Method=' . $key;
+        $query{'mvStructure.expMethod.value'} = $key;
+    }
 }
 
-make_search_query('Hello', 'ohno');
+make_search_query('Hello', 'cc');
 1;
