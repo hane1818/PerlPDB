bf550be59798052f69065b5d632fc1393842a9fe
Hane Liu <hane0131@gmail.com>
2015-12-27 21:01:03 +0800

    modify some argument presentation

--- lib/PerlPDB.pm
+++ lib/PerlPDB.pm
@@ -16,7 +16,7 @@ use URI;
 use Data::Dumper;
 
 sub make_search_query {
-    my ($key, $type)=@_;
+    my ($key, %kwargs)=@_;
 
     my @querytype = ('HoldingsQuery', 'ExpTypeQuery',
                     'AdvancedKeywordQuery','StructureIdQuery',
@@ -24,37 +24,37 @@ sub make_search_query {
                     'NoLigandQuery');
     my $querytype = join('|', @querytype);
     $querytype = '^('.$querytype.')$';
-    if ( ! $type or $type !~ /$querytype/ ) {
-        $type='AdvancedKeywordQuery'; }
+    if ( ! $kwargs{type} or $kwargs{type} !~ /$querytype/ ) {
+        $kwargs{type}='AdvancedKeywordQuery'; }
 
     my %query;
-    $query{'queryType'}=$type;
-    if ( $type =~ /^AdvancedKeywordQuery$/ ) {
+    $query{'queryType'}=$kwargs{type};
+    if ( $kwargs{type} =~ /^AdvancedKeywordQuery$/ ) {
 
         $query{'description'} = 'Text Search for: ' . $key;
         $query{'keywords'} = $key;
 
-    }elsif ( $type =~ /^NoLigandQuery$/ ) {
+    }elsif ( $kwargs{type} =~ /^NoLigandQuery$/ ) {
 
         $query{'haveLigands'} = 'yes';
 
-    }elsif ( $type =~ /^AdvancedAuthorQuery$/ ) {
+    }elsif ( $kwargs{type} =~ /^AdvancedAuthorQuery$/ ) {
 
         $query{'description'} = 'Author Name: ' . $key;
         $query{'searchType'} = 'All Authors';
         $query{'audit_author.name'} = $key;
         $query{'exactMatch'} = 'false';
 
-    }elsif ( $type =~ /^MotifQuery$/ ) {
+    }elsif ( $kwargs{type} =~ /^MotifQuery$/ ) {
 
         $query{'description'} = 'Motif Query For: ' . $key;
         $query{'motif'} = $key;
 
-    }elsif ( $type =~ /^(StructureIdQuery|ModifiedStructuresQuery)$/ ) {
+    }elsif ( $kwargs{type} =~ /^(StructureIdQuery|ModifiedStructuresQuery)$/ ) {
 
         $query{'structureIdList'} = $key;
 
-    }elsif ( $type =~ /^ExpTypeQuery$/ ) {
+    }elsif ( $kwargs{type} =~ /^ExpTypeQuery$/ ) {
 
         $query{'experimentalMethod'} = $key;
         $query{'description'} = 'Experimental Method Search : Experimental Method=' . $key;
@@ -88,16 +88,16 @@ sub search {
 
 sub search_protsym {
     # Protein symmetry search of the PDB
-    my ($point_group, $min_rmsd, $max_rmsd) = @_;
-    if( ! $min_rmsd ) { $min_rmsd = 0.0; }
-    if( ! $max_rmsd ) { $max_rmsd = 7.0; }
+    my ($point_group, %kwargs) = @_;
+    if( ! $kwargs{min_rmsd} ) { $kwargs{min_rmsd} = 0.0; }
+    if( ! $kwargs{max_rmsd} ) { $kwargs{max_rmsd} = 7.0; }
 
     my %query = (
         'queryType' => 'PointGroupQuery',
         'rMSDComparator' => 'between',
         'pointGroup' => $point_group,
-        'rMSDMin' => $min_rmsd,
-        'rMSDMax' => $max_rmsd);
+        'rMSDMin' => $kwargs{min_rmsd},
+        'rMSDMax' => $kwargs{max_rmsd});
 
     my @id_list = search(%query);
 
@@ -129,14 +129,14 @@ sub get_pdbid_info {
 
 sub get_pdbid_file {
     # Get the full PDB file associated with a PDB_ID
-    my ($pdb_id, $file_type, $compression) = @_;
-    if( ! $file_type or $file_type !~ /^(pdb|cif|xml|structfact)$/ ) { $file_type = 'pdb'; }
-    if( ! $compression or $compression =~ /^false$/i ) { $compression = 'NO'; }
-    else { $compression = 'YES'; }
+    my ($pdb_id, %kwargs) = @_;
+    if( ! $kwargs{file_type} or $kwargs{file_type} !~ /^(pdb|cif|xml|structfact)$/ ) { $kwargs{file_type} = 'pdb'; }
+    if( ! $kwargs{compression} or $kwargs{compression} =~ /^false$/i ) { $kwargs{compression} = 'NO'; }
+    else { $kwargs{compression} = 'YES'; }
 
     my $url = URI->new( 'http://www.rcsb.org/pdb/download/downloadFile.do' );
-    $url->query_form( 'fileFormat' => $file_type,
-                      'compression' => $compression,
+    $url->query_form( 'fileFormat' => $kwargs{file_type},
+                      'compression' => $kwargs{compression},
                       'structureId' => $pdb_id );
 
     my $response = LWP::UserAgent->new->get( $url );
